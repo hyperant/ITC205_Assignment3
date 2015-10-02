72de2f7922465715e77aa9e0c9aaf25beb597da3
hyperant <toja.design@gmail.com>
2015-10-02 11:14:24 +1000

    created stub class for book and member. Implemented constructor for Loan. Created all test for cases for Loans constructor.
    Signed-off-by: hyperant <toja.design@gmail.com>

--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -7,6 +7,27 @@ import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
 public class Loan implements ILoan {
+	private IBook book;
+	private IMember member;
+	
+	private Date borrowDate;
+	private Date dueDate;
+	
+	public Loan(IBook book, IMember member, Date borrowDate, Date dueDate) {
+		//Make sure we havent been passed any null values
+		if(book ==null || member ==null || borrowDate ==null || dueDate ==null) {
+			throw new IllegalArgumentException("required params can not be null");
+		}
+		
+		if(dueDate.compareTo(borrowDate) <0) {
+			throw new IllegalArgumentException("dueDate can not be less then borrowDate");
+		}
+		
+		this.book =book;
+		this.member =member;
+		this.borrowDate =borrowDate;
+		this.dueDate =dueDate;
+	}
 
 	@Override
 	public void commit(int id) {
--- src/tests/library/entities/TestLoan.java
+++ src/tests/library/entities/TestLoan.java
@@ -2,20 +2,87 @@ package tests.library.entities;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+import java.util.Calendar;
+
+import library.entities.Loan;
+import library.entities.BookStub;
+import library.entities.MemberStub;
+
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestLoan {
-
+	private IBook book;
+	private IMember member;
+	
+	private Date borrowDate;
+	private Date dueDate;
+	
+	private Calendar cal;
+	
 	@Before
 	public void setUp() throws Exception {
+		this.book =new BookStub();
+		this.member =new MemberStub();
+		
+		this.cal =Calendar.getInstance();
+		this.borrowDate =this.cal.getTime();
+		this.cal.add(Calendar.DATE, ILoan.LOAN_PERIOD);
+		this.dueDate =this.cal.getTime();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	@Test
+	public void testCreate() {
+		ILoan loan =new Loan(this.book, this.member, this.borrowDate, this.dueDate);
+		
+		assertTrue(loan instanceof Loan);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateBadParemBookNUll() {
+		ILoan loan =new Loan(null, this.member, this.borrowDate, this.dueDate);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateBadParemMemberNUll() {
+		ILoan loan =new Loan(this.book, null, this.borrowDate, this.dueDate);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateBadParemBorrowDateNUll() {
+		ILoan loan =new Loan(this.book, this.member, null, this.dueDate);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateBadParemDueDateNUll() {
+		ILoan loan =new Loan(this.book, this.member, this.borrowDate, null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCreateBadParemDueDateLessThenBorrowDate() {
+		ILoan loan =new Loan(this.book, this.member, this.dueDate, this.borrowDate);
+	}
+	
+	@Test
+	public void testCreateBadParemDueDateEqualsBorrowDate() {
+		ILoan loan =new Loan(this.book, this.member, this.borrowDate, this.borrowDate);
+		
+		assertTrue(loan instanceof Loan);
+	}
+	
+	/*
 	@Test
 	public void testCommit() {
 		fail("Not yet implemented");
@@ -55,5 +122,5 @@ public class TestLoan {
 	public void testGetID() {
 		fail("Not yet implemented");
 	}
-
+*/
 }
