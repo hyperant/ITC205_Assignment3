e2498fb973171d4af8ebd3717513e5511272df95
hyperant <toja.design@gmail.com>
2015-10-03 09:02:17 +1000

    created and setup Book class and test class for Book
    Signed-off-by: hyperant <toja.design@gmail.com>

---
+++ src/tests/library/entities/TestBook.java
@@ -0,0 +1,75 @@
+package tests.library.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBook {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/*
+	@Test
+	public void testBorrow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLoan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRepair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDispose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCallNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+*/
+}
