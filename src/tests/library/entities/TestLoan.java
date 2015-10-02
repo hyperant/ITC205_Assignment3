3f92764de1022f6e09fb805aa674b628a581eda7
hyperant <toja.design@gmail.com>
2015-10-02 10:07:40 +1000

    added test case for Loan and created Loan entitie class

---
+++ src/tests/library/entities/TestLoan.java
@@ -0,0 +1,59 @@
+package tests.library.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLoan {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCommit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComplete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOverDue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsCurrent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckOverDue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBorrower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");
+	}
+
+}
