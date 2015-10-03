d2575f4ba0a32cc8068c964dd33ae8bb2b28ca3b
hyperant <toja.design@gmail.com>
2015-10-03 10:28:00 +1000

    setup and created BookDAO.java
    Signed-off-by: hyperant <toja.design@gmail.com>

---
+++ src/tests/library/daos/TestBookDAO.java
@@ -0,0 +1,49 @@
+package tests.library.daos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBookDAO {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBookByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListBooks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBooksByAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBooksByTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindBooksByAuthorTitle() {
+		fail("Not yet implemented");
+	}
+
+}
