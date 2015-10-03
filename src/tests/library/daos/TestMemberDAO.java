104f3a244c0d519c5974e600cd4acd1d0403bdd8
hyperant <toja.design@gmail.com>
2015-10-03 18:29:38 +1000

    setup and created MemberDAO.java and TestMemberDAO.java
    Signed-off-by: hyperant <toja.design@gmail.com>

---
+++ src/tests/library/daos/TestMemberDAO.java
@@ -0,0 +1,49 @@
+package tests.library.daos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMemberDAO {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMemberByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMembersByLastName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMembersByEmailAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMembersByNames() {
+		fail("Not yet implemented");
+	}
+
+}
