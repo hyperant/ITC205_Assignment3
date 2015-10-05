92d56096e7355ef558e06001c576b228120e4b79
hyperant <toja.design@gmail.com>
2015-10-05 11:54:24 +1100

    implemented and tested all functions

--- src/tests/integration/MemberDAOandMemberIntegation.java
+++ src/tests/integration/MemberDAOandMemberIntegation.java
@@ -2,6 +2,9 @@ package tests.integration;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
+import java.util.List;
+
 import library.daos.MemberDAO;
 import library.entities.Member;
 import library.interfaces.daos.IMemberDAO;
@@ -25,6 +28,8 @@ public class MemberDAOandMemberIntegation {
 		this.memberDAO =new MemberDAO(this.helper);
 		
 		when(this.helper.makeMember(any(String.class), any(String.class), any(String.class), any(String.class), any(Integer.class))).thenReturn(this.member);
+	
+		this.memberDAO.addMember(this.member.getFirstName(), this.member.getLastName(), this.member.getContactPhone(), this.member.getEmailAddress());
 	}
 
 	@After
@@ -35,38 +40,79 @@ public class MemberDAOandMemberIntegation {
 	}
 
 	@Test
-	public void testMemberDAO() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddMember() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetMemberByID() {
-		fail("Not yet implemented");
+		assertEquals(this.member, this.memberDAO.getMemberByID(this.member.getID()));
+	}
+	
+	@Test
+	public void testGetMemberByIDNotFound() {
+		assertNull(this.memberDAO.getMemberByID(10));
 	}
 
 	@Test
 	public void testListMembers() {
-		fail("Not yet implemented");
+		List<IMember> memberList =this.memberDAO.listMembers();
+		
+		assertEquals(1, memberList.size());
+		assertEquals(this.member, memberList.get(0));
 	}
 
 	@Test
 	public void testFindMembersByLastName() {
-		fail("Not yet implemented");
+		List<IMember> memberList =this.memberDAO.findMembersByLastName(this.member.getLastName());
+		
+		assertEquals(1, memberList.size());
+		assertEquals(this.member, memberList.get(0));
+	}
+	
+	@Test
+	public void testFindMembersByLastNameNoMath() {
+		List<IMember> memberList =this.memberDAO.findMembersByLastName("I do not exist");
+		
+		assertEquals(0, memberList.size());
 	}
 
 	@Test
 	public void testFindMembersByEmailAddress() {
-		fail("Not yet implemented");
+		List<IMember> memberList =this.memberDAO.findMembersByEmailAddress(this.member.getEmailAddress());
+		
+		assertEquals(1, memberList.size());
+		assertEquals(this.member, memberList.get(0));
+	}
+	
+	@Test
+	public void testFindMembersByEmailAddressNoMatch() {
+		List<IMember> memberList =this.memberDAO.findMembersByEmailAddress("i do not exist");
+		
+		assertEquals(0, memberList.size());
 	}
 
 	@Test
 	public void testFindMembersByNames() {
-		fail("Not yet implemented");
+		List<IMember> memberList =this.memberDAO.findMembersByNames(this.member.getFirstName(), this.member.getLastName());
+		
+		assertEquals(1, memberList.size());
+		assertEquals(this.member, memberList.get(0));
 	}
-
+	
+	@Test
+	public void testFindMembersByNamesNoMatchFirstName() {
+		List<IMember> memberList =this.memberDAO.findMembersByNames("i do not exist", this.member.getLastName());
+		
+		assertEquals(0, memberList.size());
+	}
+	
+	@Test
+	public void testFindMembersByNamesNoMatchLastName() {
+		List<IMember> memberList =this.memberDAO.findMembersByNames(this.member.getFirstName(), "i do not exist");
+		
+		assertEquals(0, memberList.size());
+	}
+	
+	@Test
+	public void testFindMembersByNamesNoMatchFirstNameLastName() {
+		List<IMember> memberList =this.memberDAO.findMembersByNames("i do not exist", "i do not exist");
+		
+		assertEquals(0, memberList.size());
+	}
 }
